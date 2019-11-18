d044e324813b9d19bd4a6c757e602f5ca5265aa2
Ned-el-ch <nedelchev.niki@gmail.com>
2019-11-18 14:58:48 +0000

    Done.

--- index.js
+++ index.js
@@ -29,12 +29,12 @@ function moveDodgerLeft() {
 
 function moveDodgerRight() {
 
-    let leftNumbers = dodger.style.left.replace("px", "");
-    let left = parseInt(leftNumbers, 10);
+    let rightNumbers = dodger.style.right.replace("px", "");
+    let right = parseInt(rightNumbers, 10);
 
-    if (left > 0) {
+    if (right > 0) {
 
-        dodger.style.left = `${left + 1}px`;
+        dodger.style.left = `${right + 1}px`;
 
     }
 
